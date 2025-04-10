6226351e4bdc8f49d5cc440c9d01d9ed05f58a98
unknown <ramovic225@gmail.com>
2025-04-11 00:56:25 +0200

    getYears created

--- src/components/MyStory.jsx
+++ src/components/MyStory.jsx
@@ -1,17 +1,46 @@
-import React from 'react';
+import React from "react";
 
 const MyStory = () => {
-    return (
-        <>
-            <p className="uppercase font-semibold pb-10">My story</p>
-            <p className="text-center md:text-start leading-8 text-lg">Hello, my name is Šućo and I am 27 years old.<br/>
-                I graduated from the Faculty of Electrical Engineering in Podgorica, University of Montenegro, but even before that I was interested in programming.
-                Everything started with the C language, which I learned in second year in faculty, and I was crazy about solving logical problems in C.<br/>After graduation, I started learning web development and I fell in love with it.
-            </p>
-            <p className="text-center md:text-start leading-8 text-lg pt-5">I am a self-taught developer and I am constantly learning new things. I am currently working as a freelance web developer and I am looking for a job as a mid-level web developer. I always strive to improve my skills and I am always ready to learn new technologies. I am a very responsible person and I always try to do my best in every project I work on.
-            </p>
-        </>
-    );
+  const getYears = (birthdayDate) => {
+    const birthday = new Date(birthdayDate);
+    const today = new Date();
+
+    let years = today.getFullYear() - birthday.getFullYear();
+
+    const hasBirthdayPassedThisYear =
+      today.getMonth() > birthday.getMonth() ||
+      (today.getMonth === birthday.getMonth() &&
+        today.getDate() >= birthday.getDate());
+
+    if (!hasBirthdayPassedThisYear) years--;
+    return years;
+  };
+  const years = getYears("1996-07-27");
+
+  return (
+    <>
+      <p className="uppercase font-semibold pb-10">My story</p>
+      <p className="text-center md:text-start leading-8 text-lg">
+        Hello, my name is Šućo and I am {years} years old.
+        <br />
+        I graduated from the Faculty of Electrical Engineering in Podgorica,
+        University of Montenegro, but even before that I was interested in
+        programming. Everything started with the C language, which I learned in
+        second year in faculty, and I was crazy about solving logical problems
+        in C.
+        <br />
+        After graduation, I started learning web development and I fell in love
+        with it.
+      </p>
+      <p className="text-center md:text-start leading-8 text-lg pt-5">
+        I am a self-taught developer and I am constantly learning new things. I
+        am currently working as a freelance web developer and I am looking for a
+        job as a mid-level web developer. I always strive to improve my skills
+        and I am always ready to learn new technologies. I am a very responsible
+        person and I always try to do my best in every project I work on.
+      </p>
+    </>
+  );
 };
 
 export default MyStory;
--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -1,28 +1,34 @@
-import React from 'react';
-import {BiLogoInstagramAlt} from "react-icons/bi";
-import {FaFacebook, FaGithub, FaLinkedin} from "react-icons/fa6";
+import React from "react";
+import { BiLogoInstagramAlt } from "react-icons/bi";
+import { FaFacebook, FaGithub, FaLinkedin } from "react-icons/fa6";
 
-const Social = ({handleOpen}) => {
-    return (
-        <div className="flex gap-10 justify-center py-[2rem]">
-            <BiLogoInstagramAlt
-                size={26}
-                className="cursor-pointer hover:scale-125"
-                onClick={() => handleOpen("https://www.instagram.com/suksiiii/")}/>
-            <FaFacebook
-                size={26}
-                className="cursor-pointer hover:scale-125"
-                onClick={() => handleOpen("https://www.facebook.com/suco.ramovic/")}/>
-            <FaLinkedin
-                size={26}
-                className="cursor-pointer hover:scale-125"
-                onClick={() => handleOpen("https://me.linkedin.com/in/%C5%A1u%C4%87o-ramovi%C4%87-58a728256")}/>
-            <FaGithub
-                size={26}
-                className="cursor-pointer hover:scale-125"
-                onClick={() => handleOpen("https://github.com/Suksii")}/>
-        </div>
-    );
+const Social = ({ handleOpen }) => {
+  return (
+    <div className="flex gap-10 justify-center py-[2rem]">
+      <BiLogoInstagramAlt
+        size={26}
+        className="cursor-pointer hover:scale-125"
+        onClick={() => handleOpen("https://www.instagram.com/suksiiii/")}
+      />
+      <FaFacebook
+        size={26}
+        className="cursor-pointer hover:scale-125"
+        onClick={() => handleOpen("https://www.facebook.com/suco.ramovic/")}
+      />
+      <FaLinkedin
+        size={26}
+        className="cursor-pointer hover:scale-125"
+        onClick={() =>
+          handleOpen("https://www.linkedin.com/in/suco-ramovic-58a728256/")
+        }
+      />
+      <FaGithub
+        size={26}
+        className="cursor-pointer hover:scale-125"
+        onClick={() => handleOpen("https://github.com/Suksii")}
+      />
+    </div>
+  );
 };
 
 export default Social;
